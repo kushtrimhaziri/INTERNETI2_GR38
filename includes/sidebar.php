9b06caf4b6f163c3fa3c1fd3fc12571a2cc48ed3
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-05-11 12:05:29 +0200

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,10 +2,6 @@
 
 
 
-
-
-
-
     <div class="well">
         <h4>Blog Search</h4>
         <form action="search.php" method="post">
@@ -19,6 +15,29 @@
         </div>
         </form>
     </div>
+
+
+
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+
+                <input name="username" type="text" class="form-control" placeholder="Username: ">
+
+            </div>
+            <div class="form-group">
+
+                <input name="password" type="password" class="form-control" placeholder="Password: ">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login" type="submit" >
+                        Login
+                    </button>
+                </span>
+            </div>
+        </form>
+    </div>
+
 
 
 
@@ -38,10 +57,9 @@
                 <ul class="list-unstyled">
                     <?php
                     while ($row=mysqli_fetch_assoc($select_categories_sidebar)){
-                        $cat_id= $row['cat_id'];
                         $cat_title= $row['cat_title'];
 
-                        echo "<li><a href='category.php?category=$cat_id'> {$cat_title}</a></li>";
+                        echo "<li><a href='#'> {$cat_title}</a></li>";
                     }?>
 
                 </ul>
