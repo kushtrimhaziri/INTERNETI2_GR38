d9a1f4b9c8fcf3182715ba2f441c32710dde5667
visarahmeti <58608274+Visskey@users.noreply.github.com>
2020-05-13 22:43:45 +0200

    Add files via upload

---
+++ standings.php
@@ -0,0 +1,385 @@
+<?php include "includes/header.php";
+include "includes/db.php";
+include "includes/navigation.php";
+?>
+
+    <html>
+    <head>
+
+    </head>
+    <body>
+       <br><br>
+       <h2>Eastern Conference</h2>
+       <table class="table table-striped table-dark">
+           <thead>
+           <tr>
+               <th scope="col">#</th>
+               <th scope="col">Team</th>
+               <th scope="col">Wins</th>
+               <th scope="col">Losses</th>
+               <th scope="col">Home</th>
+               <th scope="col">Road</th>
+               <th scope="col">Last 10</th>
+               <th scope="col">Streak</th>
+
+           </tr>
+           </thead>
+           <tbody>
+           <tr>
+               <th scope="row">1</th>
+               <td><img src="images/bucks.jpg" height="25" width="25">Milwaukee Bucks</td>
+               <td>62</td>
+               <td>20</td>
+               <td>36-5</td>
+               <td>26-15</td>
+               <td>8-2</td>
+               <td>W5</td>
+           </tr>
+           <tr>
+               <th scope="row">2</th>
+               <td><img src="images/Philly.jpg" height="25" width="25">Philadelphia 76ers</td>
+               <td>58</td>
+               <td>24</td>
+               <td>36-5</td>
+               <td>22-19</td>
+               <td>7-3</td>
+               <td>L1</td>
+           </tr>
+           <tr>
+               <td>3</td>
+               <td><img src="images/celtics.jpg" height="25" width="25">Boston Celtics</td>
+               <td>54</td>
+               <td>28</td>
+               <td>24-17</td>
+               <td>30-11</td>
+               <td>6-4</td>
+               <td>W3</td>
+           </tr>
+
+           <tr>
+               <td>4</td>
+               <td><img src="images/raptors.png" height="25" width="25">Toronto Raptors</td>
+               <td>54</td>
+               <td>28</td>
+               <td>30-11</td>
+               <td>24-17</td>
+               <td>6-4</td>
+               <td>L3</td>
+           </tr>
+
+           <tr>
+               <td>5</td>
+               <td><img src="images/miami.jpg" height="25" width="25">Miami Heat</td>
+               <td>47</td>
+               <td>35</td>
+               <td>35-6</td>
+               <td>12-29</td>
+               <td>4-6</td>
+               <td>W2</td>
+           </tr>
+
+           <tr>
+               <td>6</td>
+               <td><img src="images/pacers.png" height="25" width="25">Indiana Pacers</td>
+               <td>43</td>
+               <td>39</td>
+               <td>18-23</td>
+               <td>25-16</td>
+               <td>5-5</td>
+               <td>L3</td>
+           </tr>
+
+           <tr>
+               <td>7</td>
+               <td><img src="images/nets.jpg" height="25" width="25">Brooklyn Nets</td>
+               <td>41</td>
+               <td>41</td>
+               <td>20-21</td>
+               <td>21-20</td>
+               <td>6-4</td>
+               <td>W6</td>
+           </tr>
+
+           <tr>
+               <td>8</td>
+               <td><img src="images/magic.png" height="25" width="25">Orlando Magic</td>
+               <td>38</td>
+               <td>44</td>
+               <td>24-17</td>
+               <td>14-27</td>
+               <td>4-6</td>
+               <td>W1</td>
+           </tr>
+
+           <tr>
+               <td>9</td>
+               <td><img src="images/pistons.png" height="25" width="25">Detroit Pistons</td>
+               <td>37</td>
+               <td>45</td>
+               <td>24-17</td>
+               <td>13-28</td>
+               <td>3-7</td>
+               <td>L5</td>
+           </tr>
+
+           <tr>
+               <td>10</td>
+               <td><img src="images/bulls.jpg" height="25" width="25">Chicago Bulls</td>
+               <td>34</td>
+               <td>48</td>
+               <td>15-26</td>
+               <td>19-22</td>
+               <td>4-6</td>
+               <td>W2</td>
+           </tr>
+
+           <tr>
+               <td>11</td>
+               <td><img src="images/wizards.png" height="25" width="25">Washington Wizards</td>
+               <td>32</td>
+               <td>50</td>
+               <td>16-25</td>
+               <td>16-25</td>
+               <td>2-8</td>
+               <td>L4</td>
+           </tr>
+
+           <tr>
+               <td>12</td>
+               <td><img src="images/charlotte.png" height="25" width="30">Charlotte Hornets</td>
+               <td>32</td>
+               <td>50</td>
+               <td>21-20</td>
+               <td>11-30</td>
+               <td>5-5</td>
+               <td>L5</td>
+           </tr>
+
+           <tr>
+               <td>13</td>
+               <td><img src="images/hawks.png" height="25" width="25">Atlanta Hawks</td>
+               <td>18</td>
+               <td>64</td>
+               <td>10-31</td>
+               <td>8-33</td>
+               <td>1-9</td>
+               <td>L4</td>
+           </tr>
+
+           <tr>
+               <td>14</td>
+               <td><img src="images/cavs.png" height="25" width="35">Cleveland Cavaliers</td>
+               <td>15</td>
+               <td>67</td>
+               <td>7-34</td>
+               <td>8-33</td>
+               <td>5-5</td>
+               <td>L5</td>
+           </tr>
+
+           <tr>
+               <td>15</td>
+               <td><img src="images/knicks.jpg" height="25" width="25">New York Knicks</td>
+               <td>14</td>
+               <td>68</td>
+               <td>9-32</td>
+               <td>5-36</td>
+               <td>2-8</td>
+               <td>L3</td>
+           </tr>
+           </tbody>
+       </table>
+
+       <h2>Western Conference</h2>
+
+       <table class="table table-striped table-dark">
+           <thead>
+           <tr>
+               <th scope="col">#</th>
+               <th scope="col">Team</th>
+               <th scope="col">Wins</th>
+               <th scope="col">Losses</th>
+               <th scope="col">Home</th>
+               <th scope="col">Road</th>
+               <th scope="col">Last 10</th>
+               <th scope="col">Streak</th>
+
+           </tr>
+           </thead>
+           <tbody>
+           <tr>
+               <td>1</td>
+               <td><img src="images/lakers.jpg" height="25" width="30">Los Angeles Lakers</td>
+               <td>69</td>
+               <td>13</td>
+               <td>37-4</td>
+               <td>32-9</td>
+               <td>9-1</td>
+               <td>W8</td>
+           </tr>
+
+           <tr>
+               <td>2</td>
+               <td><img src="images/clippers.jpg" height="25" width="25">Los Angeles Clippers</td>
+               <td>62</td>
+               <td>20</td>
+               <td>30-11</td>
+               <td>32-9</td>
+               <td>7-3</td>
+               <td>W6</td>
+           </tr>
+
+           <tr>
+               <td>3</td>
+               <td><img src="images/rockets.png" height="25" width="25">Houston Rockets</td>
+               <td>57</td>
+               <td>25</td>
+               <td>28-13</td>
+               <td>29-12</td>
+               <td>7-3</td>
+               <td>L1</td>
+           </tr>
+
+           <tr>
+               <td>4</td>
+               <td><img src="images/nuggets.jpg" height="25" width="25">Denver Nuggets</td>
+               <td>54</td>
+               <td>28</td>
+               <td>28-13</td>
+               <td>26-15</td>
+               <td>6-4</td>
+               <td>L1</td>
+           </tr>
+
+           <tr>
+               <td>5</td>
+               <td><img src="images/jazz.jpg" height="25" width="25">Utah Jazz</td>
+               <td>47</td>
+               <td>35</td>
+               <td>22-19</td>
+               <td>25-16</td>
+               <td>6-4</td>
+               <td>W3</td>
+           </tr>
+
+           <tr>
+               <td>6</td>
+               <td><img src="images/mavs.jpg" height="25" width="25">Dallas Mavericks</td>
+               <td>46</td>
+               <td>36</td>
+               <td>25-16</td>
+               <td>21-20</td>
+               <td>7-3</td>
+               <td>W2</td>
+           </tr>
+
+           <tr>
+               <td>7</td>
+               <td><img src="images/blazers.jpg" height="25" width="25">Portland Trail Blazers</td>
+               <td>44</td>
+               <td>38</td>
+               <td>20-21</td>
+               <td>24-17</td>
+               <td>6-4</td>
+               <td>W3</td>
+           </tr>
+
+           <tr>
+               <td>8</td>
+               <td><img src="images/spurs.jpg" height="25" width="25">San Antonio Spurs</td>
+               <td>42</td>
+               <td>40</td>
+               <td>24-17</td>
+               <td>18-23</td>
+               <td>8-2</td>
+               <td>W5</td>
+           </tr>
+
+           <tr>
+               <td>9</td>
+               <td><img src="images/wolves.png" height="25" width="25">Minnesota Timberwolves</td>
+               <td>42</td>
+               <td>40</td>
+               <td>25-16</td>
+               <td>17-24</td>
+               <td>5-5</td>
+               <td>L1</td>
+           </tr>
+
+           <tr>
+               <td>10</td>
+               <td><img src="images/thunder.png" height="25" width="30">Oklahoma City Thunder</td>
+               <td>38</td>
+               <td>44</td>
+               <td>19-22</td>
+               <td>19-22</td>
+               <td>6-4</td>
+               <td>W1</td>
+           </tr>
+
+           <tr>
+               <td>11</td>
+               <td><img src="images/suns.jpg" height="25" width="25">Phoenix Suns</td>
+               <td>37</td>
+               <td>45</td>
+               <td>19-22</td>
+               <td>18-23</td>
+               <td>4-6</td>
+               <td>L2</td>
+           </tr>
+
+           <tr>
+               <td>12</td>
+               <td><img src="images/kings.jpg" height="25" width="25">Sacramento Kings</td>
+               <td>32</td>
+               <td>50</td>
+               <td>18-23</td>
+               <td>14-27</td>
+               <td>5-5</td>
+               <td>L2</td>
+           </tr>
+
+           <tr>
+               <td>13</td>
+               <td><img src="images/pelicans.jpg" height="25" width="25">New Orleans Pelicans</td>
+               <td>18</td>
+               <td>64</td>
+               <td>10-31</td>
+               <td>8-33</td>
+               <td>1-9</td>
+               <td>L4</td>
+           </tr>
+
+           <tr>
+               <td>14</td>
+               <td><img src="images/grizzlies.jpg" height="25" width="25">Memphis Grizzlies</td>
+               <td>14</td>
+               <td>68</td>
+               <td>6-35</td>
+               <td>8-33</td>
+               <td>2-8</td>
+               <td>L6</td>
+           </tr>
+
+           <tr>
+               <td>15</td>
+               <td><img src="images/warriors.jpg" height="25" width="25">Golden State Warriors</td>
+               <td>9</td>
+               <td>73</td>
+               <td>5-36</td>
+               <td>4-37</td>
+               <td>0-10</td>
+               <td>L10</td>
+           </tr>
+           </tbody>
+       </table>
+
+    </body>
+    </html>
+
+
+
+<?php include "includes/footer.php"; ?>
+
+
+<?php
