cdbea79d479a9fe69c399de9eb71e04dbc14c560
fitoremorina <58529197+fitoremorina@users.noreply.github.com>
2020-05-13 17:47:25 +0200

    Update view_all_posts.php

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,6 +1,71 @@
+<?php 
+if(isset($_POST['checkBoxArray'])){
+foreach($_POST['checkBoxArray'] as $postValueId) {
+	
+	
+	$bulk_options= $_POST['bulk_options'];
+	
+	switch($bulk_options){
+			
+			
+		case 'published':
+			
+			
+$query="UPDATE posts SET post_status='{$bulk_options}' WHERE post_id={$postValueId}";
+			
+$update_to_published_status = mysqli_query($connection,$query);
+			
+			
+			confirmQuery($update_to_published_status);
+			
+			break;
+			
+			
+			case 'draft':
+			
+			
+$query="UPDATE posts SET post_status='{$bulk_options}' WHERE post_id={$postValueId}";
+			
+$update_to_published_status = mysqli_query($connection,$query);
+			
+			
+			confirmQuery($update_to_published_status);
+			
+			break;
+			
+			
+			
+			
+			
+			
+			
+	}
+}
+}
+?>
+
+
+<form action="" method="post">
+
 <table class="table table-bordered table-hover">
+    
+    
+    
+    <div id="bulkOptionsContainer" class="col-xs-4">
+        <select  class="form-control" name="bulk_options" id="">
+            <option value="">Select Option</option>
+            <option value="published">Publish</option>
+            <option value="draft">Draft</option>
+            <option value="delete">Delete</option>
+         </select>
+    </div>
+    <div class="col-xs-4">
+        <input type="submit" name="submit" class="btn btn-success" value="Apply">
+        <a class="btn btn-primary" href="add_post.php">Add New</a>
+    </div>
     <thead>
     <tr>
+        <th><input  id="selectAllBoxes" type="checkbox"/></th>
         <th>ID</th>
         <th>Author</th>
         <th>Title</th>
@@ -33,6 +98,10 @@
 
 
             echo "<tr>";
+            ?>
+         <td><input  class='selectBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id; ?>'></td>
+        <?php
+         
             echo "<td>$post_id</td>";
             echo "<td>$post_author</td>";
             echo "<td>$post_title</td>";
@@ -60,9 +129,14 @@
         ?>
 
 
+
+
+
+
     </tr>
     </tbody>
     </table>
+    </form>
 
 <?php
 if (isset($_GET['delete'])){
@@ -72,5 +146,4 @@ if (isset($_GET['delete'])){
    $delete_query = mysqli_query($connection,$query);
    header("Location: posts.php");
 }
-
-?>
+?>   
