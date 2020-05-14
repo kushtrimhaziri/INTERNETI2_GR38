a16eb7e8507d11c286cd36bf83f6ee647602f359
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-05-14 19:21:10 +0200

    Update admin_navigation.php

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 
-    <!-- Brand and toggle get grouped for better mobile display -->
+
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
             <span class="sr-only">Toggle navigation</span>
@@ -10,14 +10,19 @@
         </button>
         <a class="navbar-brand" href="./index.php">NBA Blog Admin</a>
     </div>
-    <!-- Top Menu Items -->
+   
     <ul class="nav navbar-right top-nav">
         <li><a href="../index.php">Home Site</a></li>
 
-
-
         <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
+                <?php
+                if (isset($_SESSION['username']))
+                {
+                    echo $_SESSION['username'];
+                }
+                ?>
+                <b class="caret"></b></a>
             <ul class="dropdown-menu">
                 <li>
                     <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
@@ -27,7 +32,8 @@
             </ul>
         </li>
     </ul>
-    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+    
+    
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
             <li>
@@ -71,5 +77,5 @@
             </li>
         </ul>
     </div>
-    <!-- /.navbar-collapse -->
+
 </nav>
