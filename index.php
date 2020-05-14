81267c1c7015c8ce362dc70d4db85d4f5f153af0
visarahmeti <58608274+Visskey@users.noreply.github.com>
2020-05-14 21:26:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,36 @@ include "includes/navigation.php";
 
         <div class="col-md-8">
             <?php
-            $query= "SELECT * FROM posts ";
+
+
+            if(isset($_GET['page'])){
+
+                $page=$_GET['page'];
+            }else{
+
+                $page="";
+            }
+
+            if($page=="" || $page==1){
+                $page_1=0;
+            }else{
+
+                $page_1=($page*3)-3;
+            }
+
+
+
+            $post_query_count = "SELECT * FROM posts";
+            $find_count = mysqli_query($connection,$post_query_count);
+            $count = mysqli_num_rows($find_count);
+
+            $count=ceil($count/5);
+
+
+
+
+
+            $query= "SELECT * FROM posts LIMIT $page_1,3";
             $select_all_posts = mysqli_query($connection,$query);
 
             while ($row=mysqli_fetch_assoc($select_all_posts)){
@@ -28,34 +57,30 @@ include "includes/navigation.php";
 
 
 
-                ?>
-
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                    ?>
 
 
-                <h2>
-                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
-                <hr>
-            
-            
-            <a href="post.php?p_id=<?php echo $post_id; ?>">
-                <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
-            </a>
-                <hr>
-                <p><?= $post_content ?></p>
-                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <h1><br></h1>
+                    <h2>
+                        <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?= $post_author ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> <?= $post_date ?></p>
+                    <hr>
 
-                <hr>
 
-            <?php }  }?>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>">
+                        <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
+                    </a>
+                    <hr>
+                    <p><?= $post_content ?></p>
+                    <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
+
+                <?php }  }?>
 
 
 
@@ -73,6 +98,20 @@ include "includes/navigation.php";
 
     </div>
     <hr>
+    <ul class="pager">
+
+        <?php
+
+        for($i=1;$i<=$count;$i++){
+
+            echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+        }
+
+        ?>
+
+
+    </ul>
+
 
 
 
@@ -80,3 +119,4 @@ include "includes/navigation.php";
 
 
 <?php
+
