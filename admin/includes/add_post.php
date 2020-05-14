82de27b2f0aada89f4d435a9e27278c6e4afc6d1
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-05-14 19:17:41 +0200

    Update add_post.php

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -25,6 +25,8 @@ if (isset($_POST['create_post'])) {
     $create_post_query=mysqli_query($connection,$query);
 
     confirmQuery($create_post_query);
+    $the_post_id=mysqli_insert_id($connection);
+    echo "<p class='bg-success'>Post Created.<a href='../post.php?p_id={$the_post_id}'>View Post</a> or<a href='posts.php?source=add_post'> Add More Posts</a></p>";
 }
 
 
@@ -32,7 +34,7 @@ if (isset($_POST['create_post'])) {
 
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
-        <label for="title">Post Category</label>
+        <label for="title">Post Title</label>
         <input type="text" class="form-control" name="title">
     </div>
     <div class="form-group">
@@ -66,8 +68,19 @@ if (isset($_POST['create_post'])) {
 
     <div class="form-group">
         <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status">
+        <select name="post_status" id="" >
+            <option value="draft">Select Options</option>
+            <option value="published">Publish</option>
+            <option value="draft">Draft</option>
+        </select>
+
+
+
+
     </div>
+
+
+
 
     <div class="form-group">
         <label for="post_image">Post Image</label>
