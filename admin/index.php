5139a044841f475a57590ec01ed8b59088006716
fitoremorina <58529197+fitoremorina@users.noreply.github.com>
2020-05-15 20:50:52 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,132 +1,246 @@
-<?php include "includes/header.php";
-include "includes/db.php";
-include "includes/navigation.php";
-?>
+<?php include "includes/admin_header.php" ?>
 
+    <div id="wrapper">
 
-    <div class="container">
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php" ?>
 
-    <div class="row">
-
-
-        <div class="col-md-8">
-            <?php
-
-
-            if(isset($_GET['page'])){
-
-                $page=$_GET['page'];
-            }else{
-
-                $page="";
-            }
-
-            if($page=="" || $page==1){
-                $page_1=0;
-            }else{
-
-                $page_1=($page*3)-3;
-            }
+        <div id="page-wrapper">
 
 
 
-            $post_query_count = "SELECT * FROM posts  WHERE post_status='published' ";
-            $find_count = mysqli_query($connection,$post_query_count);
-            $count = mysqli_num_rows($find_count);
-            
-            
-            if($count<1){
-                echo"NO POSTS";
-                  echo"<br>NO POSTS</br>";
-                                  echo"<br><h1 class='text-center'>NO POSTS AVALIABLE</h1></br>";
+            <div class="container-fluid">
 
-                 
-            }
-            else{
-
-            $count=ceil($count/5);
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome Admin
 
 
 
+                            <small> <?php echo $_SESSION['username']; ?></small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-file"></i> Blank Page
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
 
+                <!-- /.row -->
 
-            $query= "SELECT * FROM posts LIMIT $page_1,3";
-            $select_all_posts = mysqli_query($connection,$query);
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-file-text fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
 
-            while ($row=mysqli_fetch_assoc($select_all_posts)){
-                $post_id= $row['post_id'];
-                $post_title =$row['post_title'];
-                $post_author= $row['post_author'];
-                $post_date= $row['post_date'];
-                $post_image= $row['post_image'];
-                $post_content= substr($row['post_content'],0,200);
-                $post_status = $row['post_status'];
+                                        <?php
 
+                                        $query = "SELECT * FROM posts";
+                                        $select_all_post = mysqli_query($connection,$query);
+                                        $post_count = mysqli_num_rows($select_all_post);
 
+                                        echo "<div class='huge'>{$post_count}</div>";
 
-
-
-                    ?>
-
-
-                    <h1><br></h1>
-                    <h2>
-                        <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
-                    </h2>
-                    <p class="lead">
-                        by <a href="index.php"><?= $post_author ?></a>
-                    </p>
-                    <p><span class="glyphicon glyphicon-time"></span> <?= $post_date ?></p>
-                    <hr>
-
-
-                    <a href="post.php?p_id=<?php echo $post_id; ?>">
-                        <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
-                    </a>
-                    <hr>
-                    <p><?= $post_content ?></p>
-                    <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                    <hr>
-
-                <?php } 
-                
-                 }?>
+                                        ?>
 
 
 
+                                        <div>Posts</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="./posts.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+
+                                        <?php
+
+                                        $query = "SELECT * FROM comments";
+                                        $select_all_comments = mysqli_query($connection,$query);
+                                        $comment_count = mysqli_num_rows($select_all_comments);
+
+                                        echo "<div class='huge'>{$comment_count}</div>";
+
+                                        ?>
+
+                                        <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="comments.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php
+
+                                        $query = "SELECT * FROM users";
+                                        $select_all_users = mysqli_query($connection,$query);
+                                        $user_count = mysqli_num_rows($select_all_users);
+
+                                        echo "<div class='huge'>{$user_count}</div>";
+
+                                        ?>
+                                        <div> Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="users.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-list fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php
+
+                                        $query = "SELECT * FROM category";
+                                        $select_all_categories = mysqli_query($connection,$query);
+                                        $category_count = mysqli_num_rows($select_all_categories);
+
+                                        echo "<div class='huge'>{$category_count}</div>";
+
+                                        ?>
+                                        <div>Categories</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="categories.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+                <?php
+                $query = "SELECT * FROM posts WHERE post_status = 'published'";
+                $select_all_published_post = mysqli_query($connection,$query);
+                $post_published_count = mysqli_num_rows($select_all_published_post);
+
+                $query = "SELECT * FROM posts WHERE post_status = 'draft'";
+                $select_all_draft_post = mysqli_query($connection,$query);
+                $post_draft_count = mysqli_num_rows($select_all_draft_post);
+
+                $query = "SELECT * FROM comments WHERE comment_status = 'unapproved'";
+                $unapproved_comment_query = mysqli_query($connection,$query);
+                $unapproved_comment_count = mysqli_num_rows($unapproved_comment_query);
+
+                $query = "SELECT * FROM users WHERE user_role = 'subscriber'";
+                $select_all_subscribers = mysqli_query($connection,$query);
+                $subscriber_count = mysqli_num_rows($select_all_subscribers);
 
 
 
 
 
+                ?>
 
+
+
+
+                <div class="row">
+
+                    <script type="text/javascript">
+                        google.charts.load('current', {'packages':['bar']});
+                        google.charts.setOnLoadCallback(drawChart);
+
+                        function drawChart() {
+                            var data = google.visualization.arrayToDataTable([
+                                ['Data', 'Count'],
+
+                                <?php
+
+                                   $element_text = ['All Posts','Active Posts','Draft Posts','Comments','Pending Comments','Users','Subscribers','Category'];
+                                   $element_count = [$post_count,$post_published_count, $post_draft_count, $comment_count, $unapproved_comment_count, $user_count, $subscriber_count, $category_count];
+
+                                    for($i=0;$i < 8;$i++){
+
+                                        echo "['{$element_text[$i]}'" . "," . "{$element_count[$i]}],";
+
+
+                                    }
+
+
+                                ?>
+
+                            ]);
+
+                            var options = {
+                                chart: {
+                                    title: '',
+                                    subtitle: '',
+                                }
+                            };
+
+                            var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                            chart.draw(data, google.charts.Bar.convertOptions(options));
+                        }
+                    </script>
+
+                <div id="columnchart_material" style="width: auto; height: 500px;"></div>
+
+                    </div>
+
+
+
+            </div>
+            <!-- /.container-fluid -->
 
         </div>
+        <!-- /#page-wrapper -->
 
-
-        <?php include "includes/sidebar.php"; ?>
-
-    </div>
-    <hr>
-    <ul class="pager">
-
-        <?php
-
-        for($i=1;$i<=$count;$i++){
-
-            echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-        }
-
-        ?>
-
-
-    </ul>
-
-
-
-
-<?php include "includes/footer.php"; ?>
-
-
-<?php
+<?php include "includes/admin_footer.php" ?>
