9e72cae82d95eab6b53b3e6ca63824f68648b924
gencqelaj <58531203+gencqelaj@users.noreply.github.com>
2020-05-16 00:22:49 +0200

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,20 +1,23 @@
 <div class="col-md-4">
 
-
+    <br><br><br><br><br><br>
 
     <div class="well">
         <h4>Blog Search</h4>
         <form action="search.php" method="post">
-        <div class="input-group">
-            <input name="search" type="text" class="form-control">
-            <span class="input-group-btn">
+            <div class="input-group">
+                <input name="search" type="text" class="form-control">
+                <span class="input-group-btn">
                             <button name="submit" class="btn btn-default" type="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
-        </div>
+            </div>
         </form>
     </div>
+
+
+
 
 
 
@@ -22,17 +25,14 @@
         <h4>Login</h4>
         <form action="includes/login.php" method="post">
             <div class="form-group">
-
-                <input name="username" type="text" class="form-control" placeholder="Username: ">
-
+                <input name="username" type="text" class="form-control" placeholder="Enter Username">
             </div>
-            <div class="form-group">
 
-                <input name="password" type="password" class="form-control" placeholder="Password: ">
+            <div class="input-group">
+                <input name="password" type="password" class="form-control" placeholder="Enter Password">
                 <span class="input-group-btn">
-                    <button class="btn btn-primary" name="login" type="submit" >
-                        Login
-                    </button>
+            <button class="btn btn-primary" name="login" type="submit">Submit</button>
+
                 </span>
             </div>
         </form>
@@ -73,6 +73,7 @@
         <!-- /.row -->
     </div>
 
+    <!-- Side Widget Well -->
     
-</div>
 
+</div>
