925019cea0c33cb575f4c71d2095a8153ad1faa7
fitoremorina <58529197+fitoremorina@users.noreply.github.com>
2020-05-15 20:45:24 +0200

    Update view_all_posts.php

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+include("delete_modal.php");
+
 if(isset($_POST['checkBoxArray'])){
 foreach($_POST['checkBoxArray'] as $checkBoxValue) {
 	
@@ -150,7 +152,8 @@ foreach($_POST['checkBoxArray'] as $checkBoxValue) {
             echo "<td>$post_comment_count</td>";
             echo "<td>$post_date</td>";
             echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-            echo "<td><a onclick=\"javascript: return confirm('Are you sure you want to delete?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
+			echo "<td><a rel='$post_id' href='javascript:void(0)' class='delete_link'>Delete</a></td>";
+           // echo "<td><a onclick=\"javascript: return confirm('Are you sure you want to delete?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
 
 
             echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
@@ -158,6 +161,11 @@ foreach($_POST['checkBoxArray'] as $checkBoxValue) {
             echo "</tr>";
         }
         ?>
+
+
+
+
+
 
     </tr>
     </tbody>
@@ -181,3 +189,18 @@ if (isset($_GET['reset'])){
     header("Location: posts.php");
 }
 ?>   
+
+
+<script>
+	$(document).ready(function(){
+					
+       $(".delete_link").on('click',function(){
+		var id = $(this).attr("rel");
+		   var delete_url="posts.php?delete="+id+" ";
+		 $(".modal_delete_link").attr("href", delete_url);
+		 $("#myModal").modal('show');
+
+		
+	});
+	  });
+</script>
