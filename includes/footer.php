76a5eca9daede4238e91dc4af8c052d5b79f4349
visarahmeti <58608274+Visskey@users.noreply.github.com>
2020-05-17 23:48:04 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,21 +1,27 @@
 <footer>
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Copyright &copy; Your Website 2014</p>
-        </div>
-        <!-- /.col-lg-12 -->
+
+
+
+    <div class="weather-container">
+        <img class="icon">
+        <p class="weather"></p>
+        <p class="temp"></p>
+        <p class="city"></p>
     </div>
-    <!-- /.row -->
+
+    </div>
+
 </footer>
 
 </div>
-<!-- /.container -->
 
-<!-- jQuery -->
 <script src="js/jquery.js"></script>
 
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="api.js"></script>
+<link rel="stylesheet" href="weather.css">
 </body>
+
 </html>
