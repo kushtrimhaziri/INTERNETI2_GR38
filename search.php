6c71c9855196705782e294bba926fb4a8c8c6a90
gencqelaj <58531203+gencqelaj@users.noreply.github.com>
2020-05-17 23:54:52 +0200

    Update search.php

--- search.php
+++ search.php
@@ -2,31 +2,21 @@
 include "includes/db.php";
 include "includes/navigation.php";
 ?>
-
-
 <div class="container">
-
     <div class="row">
-
-
         <div class="col-md-8">
             <?php
-
             if (isset($_POST['submit'])) {
                 $search = $_POST['search'];
                 $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
                 $search_query = mysqli_query($connection, $query);
-
                 if (!$search_query) {
                     die("QUERY FAILED" . mysqli_errno());
                 }
-
                 $count = mysqli_num_rows($search_query);
-
                 if ($count == 0) {
                     echo "<h1>No result</h1>";
                 } else {
-
             while ($row=mysqli_fetch_assoc($search_query)){
                 $post_title= $row['post_title'];
                 $post_author= $row['post_author'];
@@ -34,13 +24,10 @@ include "includes/navigation.php";
                 $post_image= $row['post_image'];
                 $post_content= $row['post_content'];
                 ?>
-
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
-
-
                 <h2>
                     <a href="#"><?= $post_title ?></a>
                 </h2>
@@ -53,21 +40,12 @@ include "includes/navigation.php";
                 <hr>
                 <p><?= $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
                 <hr>
-
             <?php }
                 }
-
             }?>
         </div>
-
-
         <?php include "includes/sidebar.php"; ?>
-
     </div>
     <hr>
-
-
-
     <?php include "includes/footer.php"; ?>
