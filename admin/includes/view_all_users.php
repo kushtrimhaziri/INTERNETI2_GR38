a02c99294dbde78c784e5d53b2092f847d2fc290
visarahmeti <58608274+Visskey@users.noreply.github.com>
2020-05-17 23:44:16 +0200

    Update view_all_users.php

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -106,15 +106,19 @@ if (isset($_GET['change_to_subscriber'])){
 
 
 if (isset($_GET['delete'])){
-    $the_user_id= $_GET['delete'];
+    if(isset($_SESSION['user_role'])){
 
-    $query = "DELETE FROM users WHERE user_id={$the_user_id}";
-    $delete_user_query = mysqli_query($connection,$query);
+        if ($_SESSION['user_role']=='admin') {
 
-    header("Location: users.php");
+            $the_user_id = mysqli_real_escape_string($connection, $_GET['delete']);
 
+            $query = "DELETE FROM users WHERE user_id={$the_user_id}";
+            $delete_user_query = mysqli_query($connection, $query);
 
-}
+            header("Location: users.php");
+
+        }
+}}
 
 
 ?>
