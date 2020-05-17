96d6d13d7a0bfea7ac63e4117ee3bb8bb93b5fb3
gencqelaj <58531203+gencqelaj@users.noreply.github.com>
2020-05-17 23:48:02 +0200

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,7 +1,5 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
-
-
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -17,42 +15,55 @@
             <ul class="nav navbar-nav">
                 <?php
                 $query= "SELECT * FROM category";
-                $select_all_categories = mysqli_query($connection,$query);
-
-                while ($row=mysqli_fetch_assoc($select_all_categories)){
+                $select_all_categories_query = mysqli_query($connection,$query);
+                while ($row=mysqli_fetch_assoc($select_all_categories_query)){
                     $cat_title= $row['cat_title'];
-
-                    echo "<li><a href='#'> {$cat_title}</a></li>";
+                    $cat_id=$row['cat_id'];
+                    $category_class='';
+                    $registration_class = '';
+                    $contact_class='';
+                    $standings_class='';
+                    $pageName=basename($_SERVER['PHP_SELF']);
+                    $registration='registration.php';
+                    $contact='contact.php';
+                    $standings='standings.php';
+                    if(isset($_GET['category']) && $_GET['category']==$cat_id ){
+                        $category_class='active';
+                    }else if($pageName==$registration){
+                        $registration_class='active';
+                    }
+                    else if($pageName==$contact){
+                        $contact_class='active';
+                    }
+                    else if($pageName==$standings){
+                        $standings_class='active';
+                    }
                 }
-
-
                 ?>
-                <li>
-                    <a href="standings.php">Standings</a>
+                <li class='<?php echo $standings_class; ?>'>
+                    <a href="standings">Standings</a>
                 </li>
-
                 <li>
                     <a href="admin">Admin</a>
                 </li>
-                <li>
-                    <a href="registration.php">Registration</a>
+                <li class='<?php echo $registration_class; ?>'>
+                    <a href="registration">Registration</a>
                 </li>
-                <li>
-                    <a href="contact.php">Contact</a>
+                <li class='<?php echo $contact_class; ?>'>
+                    <a href="contact">Contact</a>
                 </li>
-
-				
-               <?php
+                <li >
+                    <a href="profile.php">Profile</a>
+                </li>
+                <?php
                 if(isset($_SESSION['user_role']))
 				{
                        if (isset($_GET['p_id'])){
                       $the_post_id = $_GET['p_id'];
 						   echo "<li><a href='admin/posts.php?source=edit_post&p_id=
 						   {$the_post_id}'>Edit Post</a></li>";
-                       
                        }
                 }
-                
                   ?>
             </ul>
         </div>
