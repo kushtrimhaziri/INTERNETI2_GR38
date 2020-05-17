1a082af27cdbeef85e003b5e1ad5b2c1909f4d35
gencqelaj <58531203+gencqelaj@users.noreply.github.com>
2020-05-17 23:46:44 +0200

    Update admin_navigation.php

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-
-
+    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
             <span class="sr-only">Toggle navigation</span>
@@ -10,10 +9,9 @@
         </button>
         <a class="navbar-brand" href="./index.php">NBA Blog Admin</a>
     </div>
-   
+    <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
         <li><a href="../index.php">Home Site</a></li>
-
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
                 <?php
@@ -32,14 +30,12 @@
             </ul>
         </li>
     </ul>
-    
-    
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
             <li>
                 <a href="./index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
             </li>
-
             <li>
                 <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="posts_dropdown" class="collapse">
@@ -51,15 +47,12 @@
                     </li>
                 </ul>
             </li>
-
             <li>
                 <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
             </li>
-
             <li class="">
                 <a href="./comments.php"><i class="fa fa-fw fa-file"></i> Comments</a>
             </li>
-
             <li>
                 <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="demo" class="collapse">
@@ -71,11 +64,10 @@
                     </li>
                 </ul>
             </li>
-
             <li>
                 <a href="profile.php"><i class="fa fa-fw fa-dashboard"></i> Profile </a>
             </li>
         </ul>
     </div>
-
+    <!-- /.navbar-collapse -->
 </nav>
