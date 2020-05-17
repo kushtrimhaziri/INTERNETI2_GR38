ba3edd3a0b04a992cec482fb9d941df5a6f9936b
visarahmeti <58608274+Visskey@users.noreply.github.com>
2020-05-17 23:54:05 +0200

    Update post.php

--- post.php
+++ post.php
@@ -21,22 +21,36 @@ include "includes/navigation.php";
                 die("QUERY FAILED");
             }
 
+            if(isset($_SESSION['user_role'])&&$_SESSION['user_role']=='admin'){
 
-            $query= "SELECT * FROM posts WHERE post_id =$the_post_id";
-            $select_all_posts = mysqli_query($connection,$query);
+                $query= "SELECT * FROM posts WHERE post_id =$the_post_id";
 
-            while ($row=mysqli_fetch_assoc($select_all_posts)){
+            }
+            else{
+
+                $query= "SELECT * FROM posts WHERE post_id =$the_post_id AND post_status='published'";
+            }
+
+            $select_all_posts_query = mysqli_query($connection,$query);
+
+             if(mysqli_num_rows($select_all_posts_query)<1){
+                echo "<br><br><br>";
+                echo "<h1>No posts available</h1>";
+            }else{
+
+
+            while ($row=mysqli_fetch_assoc($select_all_posts_query)){
                 $post_title= $row['post_title'];
                 $post_author= $row['post_author'];
                 $post_date= $row['post_date'];
                 $post_image= $row['post_image'];
                 $post_content= $row['post_content'];
                 ?>
+                <br>
+                <br>
+                <br>
+                <br>
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
 
 
                 <h2>
@@ -54,9 +68,9 @@ include "includes/navigation.php";
 
                 <hr>
 
-            <?php }}
-            else
-                header("Location: index.php");
+            <?php }
+
+
                 ?>
 
             <!-- Blog Comments -->
@@ -166,7 +180,13 @@ include "includes/navigation.php";
 
 
 
-            <?php  }  ?>
+            <?php  }}}
+
+            else {
+                header("Location: index.php");
+            }
+
+            ?>
 
 
 
